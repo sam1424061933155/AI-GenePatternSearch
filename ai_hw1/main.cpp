791f6ae89e8d09b224b56c39acca28a3b2898c62
sam1424061933155 <sam1424061933155@gmail.com>
2017-04-27 20:01:59 +0800

    4/27

--- ai_hw1/main.cpp
+++ ai_hw1/main.cpp
@@ -41,10 +41,12 @@ bool checkMutation(string pattern, string temp){
             error++;
         }
         if(error>5){
+            cout << "error = "<<error<<" return false"<<endl;
             return false;
         }
     }
-    
+    cout << "error = "<<error<<"return true"<<endl;
+
     return true;
 }
 char mapChar(int value){
@@ -122,7 +124,7 @@ int main(int argc, const char * argv[]) {
 
     ifstream inputFile1,inputFile2,inputFile3;
     inputFile1.open("/Users/sam/Desktop/course/AI/ai_hw1/ai_hw1/q1.data",ifstream::in);
-    inputFile2.open("/Users/sam/Desktop/course/AI/ai_hw1/ai_hw1/q2.data",ifstream::in);
+    inputFile2.open("/Users/sam/Desktop/course/AI/ai_hw1/ai_hw1/ex1_5_mutates.data",ifstream::in);
 
     string temp_line;
     string line[50];
@@ -182,117 +184,53 @@ int main(int argc, const char * argv[]) {
     while(getline(inputFile2,temp_line))
     {
         line[i]=temp_line.substr(0,temp_line.length()-1);
-        
-        // count the number of each char
-        for(int j=0;j<line[i].size();j++){
-            //cout <<"line["<<i<<"]["<<j<<"] = "<<line[i][j]<<endl;
-            if(count.find(line[i][j])!=count.end()){
-                count[line[i][j]]++;
-            }else{
-                count[line[i][j]]=1;
-            }
-        }
-        //cout <<"next line"<<endl;
         i++;
     }
-    cout << "count a "<<count['A']<<endl;
-    cout << "count t "<<count['T']<<endl;
-    cout << "count c "<<count['C']<<endl;
-    cout << "count g "<<count['G']<<endl;
 
-    //int sum = count['A']+count['T']+count['C']+count['G'];
-    int a = 15*count['A']/50000;
-    int t = 15*count['T']/50000+1;
-    int c = 15*count['C']/50000;
-    int g = 15*count['G']/50000;
-
-    cout << "a = "<<a<<" ,t = "<<t<<" ,c = "<<c<<" ,g = "<<g<<endl;
-    int max =-100;
+   
+    //int max =-100;
     string max_s;
+    int isＭutate;
     
     for(int j=0;j<=985;j++){
-        //cout <<"j is "<<j<<endl;
+        cout <<"j = "<<j<<endl;
+        isＭutate=0;
         
-        string temp = line[0].substr(j,15);  //substr(starPos,length)
-        
-        
-        if(pattern.find(temp)!=pattern.end()){  //find in map   //maybe be contain more than one in other control sequence
-            //do nothing
-        }else{  //not in map
+        string temp = line[0].substr(j,15);
+        cout << "temp is = "<<temp<<endl;
+        cout <<"fuck you"<<endl;
 
-            for(int k=1;k<50;k++){
-                if(line[k].find(temp)!=string::npos){
-                    if(pattern.find(temp)==pattern.end()){
-                        pattern[temp]=1;
-                    }else{
-                        pattern[temp]++;
-                    }
-                }else if(line[k].find(temp)==string::npos){
-                    //do nothing
-                }
-            }
-        }
-        if(pattern[temp]>max){
-            max=pattern[temp];
-            max_s=temp;
-        }
-        
-    }
-    cout <<"string = "<<max_s<<" time = "<<max<<endl;
-    
-    string goal_pattern2=createRandomPattern(a,t,c,g);
-
-    
-    
-    
-    vector<sig_pattern> v_sig_pattern;
-    int find=0;
-    int find_pattern=0;
-    int time=0;
-    string temp_goal;
-    int temp_count=0;
-    int max_count=-100;
-    
-    while(find_pattern==0){
-        time++;
-        cout << "this is "<<time<<" time"<<endl;
-        if(time!=1){
-            max_s=mutation(max_s);
-        }
-        
-        for(int j=0;j<50;j++){
-            find=0;
-            //cout << "line "<<j+1<<endl;
-            if(line[j].find(max_s)!=string::npos){
-                v_sig_pattern.push_back( sig_pattern( max_s, ( line[j].find(max_s) ) ));
+        for(int k=0;k<=985;k++){
+            string compare_temp=line[1].substr(k,15);
+            cout << "temp = "<<temp<<" compare = "<<compare_temp<<endl;
+            if(j!=k && checkMutation(temp, compare_temp)){
+                isＭutate=1;
+                cout <<"k = "<<k<<" in break"<<endl;
+                break;
             }else{
-                for(int k=0;k<985;k++){
-                    string temp = line[j].substr(k,15);  //substr(starPos,length)
-                    if(checkMutation(max_s, temp)){  //mutation less than 5
-                        v_sig_pattern.push_back( sig_pattern(temp,k));
-                        find=1;
-                        //temp_count++;
-                        break;
-                    }
-                    
-                }
-                if(find==0){
-                    v_sig_pattern.clear();   // not find significant patter in line
-                    /*if(temp_count>max_count){
-                        goal_pattern2=max_s;
-                    }
-                    temp_count=0;*/
-                    break;
-                }
+                cout<<"k = "<<k<<" not break"<<endl;
             }
         }
-        if(v_sig_pattern.size()==50){
-            find_pattern=1;
+        if(isＭutate==0){
+            pattern[temp]=1;
         }
-        
-
+       
+        cout <<"fuck you"<<endl;
     }
-    cout << "find goal pattern , and pattern is  "<<max_s<<endl;
+    
+    
+    map<string, int>::iterator iter;
+    cout <<"pattern size "<<pattern.size()<<endl;
+
+    for(iter = pattern.begin(); iter != pattern.end(); iter++){
+        cout << "pattern is = "<<iter->first <<endl;
+        
+    }
+    
+    
+
+    
+    
     
     
     
